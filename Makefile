810c013b8b1dafe37cf49c42d8d20610ab1fd4b6
Choongmin Lee <choongmin@me.com>
2013-09-10 08:46:55 +0900

    Fix annoying warnings when installing audiodiff

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: init test docs release
+.PHONY: init test dist release docs clean
 
 init:
 	pip install -r requirements.txt
@@ -6,8 +6,19 @@ init:
 test:
 	py.test
 
-release: test
+dist: clean
+	python setup.py sdist
+
+release: test clean
 	python setup.py sdist upload
 
 docs:
 	$(MAKE) -C docs html
+
+clean:
+	rm -rf audiodiff.egg-info
+	rm -rf build
+	rm -rf dist
+	rm -rf docs/_build
+	find . -type f -name '*.pyc' -exec rm {} \;
+	find . -type d -name '__pycache__' -maxdepth 1 -exec rm -rf {} \;
--- setup.py
+++ setup.py
@@ -1,59 +1,48 @@
 import os
 import re
 import sys
-from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
 
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-# Parse version since we can't import the package
-# due to dependencies
+packages = [
+    'audiodiff'
+]
+
+requires = [
+    "mutagenwrapper == 0.0.4",
+    "termcolor == 1.1.0",
+]
+
+# Parse version since we can't import the package due to dependencies
 def getversion():
     with open('audiodiff/__init__.py') as f:
         text = f.read()
         m = re.search("^__version__ = '(.*)'$", text, re.M)
         return m.group(1)
 
-
-# Utility function to read the README file.
-# Used for the long_description.  It"s nice, because now 1) we have a top level
-# README file and 2) it"s easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-
-class PyTest(TestCommand):
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.test_args)
-        sys.exit(errno)
-
-
 setup(
     name = "audiodiff",
     version = getversion(),
+    description = "commandline tool to compare audio files",
+    long_description = open("README.rst").read(),
     author = "Choongmin Lee",
     author_email = "choongmin@me.com",
-    description = "commandline tool to compare audio files",
-    keywords = "lossless audio metadata comparison",
-    license = "MIT License",
     url = "https://github.com/clee704/audiodiff",
-    packages = find_packages(),
-    install_requires = [
-        "mutagenwrapper == 0.0.4",
-        "termcolor == 1.1.0",
-    ],
-    long_description = read("README.rst"),
+    packages = packages,
+    install_requires = requires,
+    entry_points = {
+      "console_scripts": [
+        'audiodiff = audiodiff:main_func',
+      ],
+    },
+    license = open('LICENSE').read(),
+    keywords = "lossless audio metadata comparison",
     classifiers = [
         # Full list is here: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        "Development Status :: 1 - Planning",
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Intended Audience :: End Users/Desktop",
         "License :: OSI Approved :: MIT License",
@@ -61,11 +50,4 @@ setup(
         "Topic :: Multimedia :: Sound/Audio :: Analysis",
         "Topic :: Utilities",
     ],
-    tests_require = ["pytest"],
-    cmdclass = {"test": PyTest},
-    entry_points = {
-      "console_scripts": [
-        'audiodiff = audiodiff:main_func',
-      ],
-    }
 )
