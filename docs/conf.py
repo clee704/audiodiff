bef6a447ab6c53de6cb56cc63836fa4fef5b7786
Choongmin Lee <choongmin@me.com>
2014-02-12 09:54:57 +0900

    Use sphinx.ext.viewcode

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,11 @@ from audiodiff import __version__
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx',
+]
 
 intersphinx_mapping = {
     'python': ('http://docs.python.org/2.7', None),
